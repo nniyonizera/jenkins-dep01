0ce12c12c4c2034f6fdc035a67d811949f04ba9a
Norbert <norniyprof@gmail.com>
2022-10-08 09:52:39 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsListComponent } from './students-list/students-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'students-list',component: StudentsListComponent},
+{path:'',component: StudentsListComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/data/student-class.model.spec.ts
@@ -0,0 +1,7 @@
+import { StudentClass } from './student-class.model';
+
+describe('StudentClass', () => {
+  it('should create an instance', () => {
+    expect(new StudentClass()).toBeTruthy();
+  });
+});
